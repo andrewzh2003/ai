51f2e63945525c75717bab1aee81df0c5d081505
andrewzh2003 <97128418+andrewzh2003@users.noreply.github.com>
2023-11-12 17:07:02 -0800

    Update StudentAI.py

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -5,23 +5,16 @@ from BoardClasses import Board
 #Students can modify anything except the class name and exisiting functions and varibles.
 class StudentAI():
 
-    def __init__(self,col,row,p):
+    def __init__(self, col, row, p):
         self.col = col
         self.row = row
         self.p = p
-        self.board = Board(col,row,p)
+        self.board = Board(col, row, p)
         self.board.initialize_game()
         self.color = ''
-        self.opponent = {1:2,2:1}
+        self.opponent = {1: 2, 2: 1}
         self.color = 2
-        self.piece_values = {
-            "pawn": 1,
-            "knight": 3,
-            "bishop": 3,
-            "rook": 5,
-            "queen": 9,
-            "king": 0
-        }
+
     def get_move(self,move):
         if len(move) != 0:
             self.board.make_move(move,self.opponent[self.color])
@@ -31,32 +24,37 @@ class StudentAI():
         # index = randint(0,len(moves)-1)
         # inner_index =  randint(0,len(moves[index])-1)
         # move = moves[index][inner_index]
-        move = self.minMax(self.color, 3)[1]
+        moves = self.board.get_all_possible_moves(self.color)
+        best_move = moves[0][0]
+        move = self.minMax(self.color, 3, best_move, float('-inf'), float('inf'))[1]
         self.board.make_move(move,self.color)
         return move
-    def minimax(self, color, depth):
+    def minMax(self, color, depth, best_move, alpha, beta) :
         if depth == 0:
-            return self.evaluate_board(color), None
+            return self.evaluate_board(color), best_move
 
-        best_score = float('-inf') if player == self.color else float('inf')
-        best_move = None
-
-        potential_moves = moves = self.board.get_all_possible_moves(self.color)
+        potential_moves = self.board.get_all_possible_moves(color)
+        best_score = float('-inf') if color == self.color else float('inf')
 
         for move in potential_moves:
             for sub_move in move:
                 self.board.make_move(sub_move, color)
-                cur_color = self.opponent[self.color] if color == self.color else self.color
-                temp_score = minimax(cur_color, depth-1, )
 
-                opponent_score, _ = self.minMax(self.opponent[color], depth - 1)
-
-                if (player == self.color and opponent_score > best_score) or \
-                        (player == self.opponent[self.color] and opponent_score < best_score):
-                    best_score = opponent_score
-                    best_move = sub_move
+                if color == self.color:
+                    temp_score = self.minMax(self.opponent[self.color], depth - 1, best_move, alpha, beta)[0]
+                    if temp_score > best_score:
+                        best_score = temp_score
+                        best_move = sub_move
+                        alpha = max(alpha, best_score)
+                elif color == self.opponent[self.color]:
+                    temp_score = self.minMax(self.color, depth - 1, best_move, alpha, beta)[0]
+                    if temp_score < best_score:
+                        best_score = temp_score
+                        best_move = sub_move
+                        beta = min(beta, best_score)
 
                 self.board.undo()
+
         return best_score, best_move
 
     def evaluate_board(self, color):
@@ -66,15 +64,18 @@ class StudentAI():
 
         # evaluate score for the whole board
         for i in range(self.col):
-            for j in range(self.rows):
+            for j in range(self.row):
                 temp_points = 0
-                piece = self.board[i][j]
-                temp_points += self.piece_values[str(piece)]
+                piece = self.board.board[i][j]
+                if piece.is_king == True:
+                    temp_points += 10
+                else:
+                    temp_points += 5
 
                 if color == 1:
-                    temp_points += ((self.row - r) / self.row) * 10
+                    temp_points += ((self.row - j) / self.row) * 10
                 else:
-                    temp_points += ((r / self.row) * 10)
+                    temp_points += ((j / self.row) * 10)
 
                 if piece.get_color() == color:
                     player_points += temp_points
@@ -83,5 +84,3 @@ class StudentAI():
 
 
         return player_points - opponent_points + randint(0,10)
-
-
